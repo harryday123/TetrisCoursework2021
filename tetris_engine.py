b95d23b8e97cc74101e3f3eb1c226482ff19b1f9
Harry Day <contact@harryday.xyz>
2021-11-15 19:36:16 +0000

    Added Set Game Options Function

--- tetris_engine.py
+++ tetris_engine.py
@@ -5,7 +5,7 @@ guidelines. More info at: https://tetris.fandom.com/wiki/Tetris_Guideline
 """
 
 
-class TetrisEngine():
+class TetrisEngine:
     """An engine to run the classic Tetris Game.
 
     This class contains all the relevant code in order to have a functioning
@@ -13,7 +13,11 @@ class TetrisEngine():
     a GUI.
 
     Attributes:
-        debug: Debug mode shows output messages and logs to aid with debugging
+        _debug:
+            Debug mode shows output messages and logs to aid with debugging
+        _game_options:
+            A dictionary of game options.
+            Current keys are next_queue, hold_queue and ghost_piece
     """
 
     def __init__(self, debug=False):
@@ -22,8 +26,39 @@ class TetrisEngine():
         Args:
             debug: Determines whether to run the engine in debug mode
         """
-        self.debug = debug
+        if debug:
+            print("DEBUG: Running TetrisEngine.__init__()")
+
+        self._debug = debug
+        self.set_game_options()
+
+    def set_game_options(
+        self,
+        next_queue=6,
+        hold_queue=True,
+        ghost_piece=True
+    ):
+        """Set the options for the game engine.
+
+        Args:
+            next_queue:
+                How many Tetriminos to display in the next queue.
+                Int 1-6
+            hold_queue:
+                Sets whether the user is able to hold a Tetrimino or not
+                Defaults to True
+            ghost_piece:
+                Whether to show the ghost piece
+                Defaults to True
+        """
+        self._game_options = {
+            "next_queue": next_queue,
+            "hold_queue": hold_queue,
+            "ghost_piece": ghost_piece
+        }
+        if self._debug:
+            print("DEBUG: Set Game Options to", self._game_options)
 
 
 if __name__ == "__main__":
-    TetrisEngine(debug=True)
+    engine = TetrisEngine(debug=True)
