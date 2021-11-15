c841a03666af1a1891da07a10d3e597aeaa64f89
Harry Day <contact@harryday.xyz>
2021-11-15 18:18:56 +0000

    Initial Setup for the Tetris Game

---
+++ main.py
@@ -0,0 +1,36 @@
+"""Harry Day (k14454hd) COMP16321 Coursework.
+
+This coursework is an adaptation of the classic Tetris game written in Tkinter.
+"""
+# Imports
+import tkinter as tk
+
+
+class TetrisApplication(tk.Frame):
+    """The main frame for the Tetris game.
+
+    Contains all the relevant GUI for the tetris game.
+
+    Attributes:
+        parent: The parent of the frame (normally the root)
+    """
+
+    def __init__(self, parent, *args, **kwargs):
+        """Initialise the TetrisApplication object.
+
+        Initialises the class with a parent and any arguments
+        that have been provided
+
+        Args:
+            parent: The parent to this frame (normally the root)
+            *args: A variable length list of arguments
+            **kwargs: A variable length dictionary of keyword arguments
+        """
+        super().__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    TetrisApplication(root).pack()
+    root.mainloop()
