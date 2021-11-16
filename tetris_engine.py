a67a3700187a5e86203a7bf8dfe5f751bbcfeb43
Harry Day <contact@harryday.xyz>
2021-11-16 00:17:31 +0000

    Add Leaderboard I/O
    Create functions which allow the leaderboard to be read and also written 
    to.
    Additionally also created a .gitignore

--- tetris_engine.py
+++ tetris_engine.py
@@ -4,6 +4,9 @@ The module contains the Tetris Game Engine adapted from the official Tetris
 guidelines. More info at: https://tetris.fandom.com/wiki/Tetris_Guideline
 """
 
+# Imports
+from csv import DictReader, DictWriter, reader
+
 
 class TetrisEngine:
     """An engine to run the classic Tetris Game.
@@ -18,9 +21,11 @@ class TetrisEngine:
         _game_options:
             A dictionary of game options.
             Current keys are next_queue, hold_queue and ghost_piece
+        _leaderboard:
+            The relative file path to the leaderboard file.
     """
 
-    def __init__(self, debug=False):
+    def __init__(self, debug=False, scoreboard="leaderboard.csv"):
         """Initialise the Game Engine with the given options.
 
         Args:
@@ -30,21 +35,17 @@ class TetrisEngine:
             print("DEBUG: Running TetrisEngine.__init__()")
 
         self._debug = debug
+        self._leaderboard = scoreboard
         self.set_game_options()
 
-    def set_game_options(
-        self,
-        next_queue=6,
-        hold_queue=True,
-        ghost_piece=True
-    ):
+    def set_game_options(self, next_queue=6, hold_on=True, ghost_piece=True):
         """Set the options for the game engine.
 
         Args:
             next_queue:
                 How many Tetriminos to display in the next queue.
                 Int 1-6
-            hold_queue:
+            hold_on:
                 Sets whether the user is able to hold a Tetrimino or not
                 Defaults to True
             ghost_piece:
@@ -53,12 +54,52 @@ class TetrisEngine:
         """
         self._game_options = {
             "next_queue": next_queue,
-            "hold_queue": hold_queue,
+            "hold_queue": hold_on,
             "ghost_piece": ghost_piece
         }
         if self._debug:
             print("DEBUG: Set Game Options to", self._game_options)
 
+    def read_leaderboard(self):
+        """Return the saved leaderboard.
+
+        Read the leaderboard from the engine's leaderboard file and return it.
+
+        Returns:
+            A list of tuples. The first item being the player's initials,
+            the second their score.
+        """
+        output_list = []
+        with open(self._leaderboard, mode='r') as f:
+            reader = DictReader(f)
+            for row in reader:
+                output_list.append((row["Initials"], int(row["Score"])))
+
+        output_list.sort(key=lambda x: x[1])
+        if self._debug:
+            print("DEBUG: Leaderboard Generated: ", output_list)
+
+        return output_list
+
+    def add_to_leaderboard(self, initials, score):
+        """Add an entry to the leaderboard.
+
+        Writes a new row in the engine's leaderboard file with the given
+        initials and score.
+
+        Args:
+            initials:
+                A string containing the initials of the player
+            score:
+                An integer representing the player's score
+        """
+        with open(self._leaderboard, mode='a') as f:
+            fieldnames = ["Initials", "Score"]
+            writer = DictWriter(f, fieldnames=fieldnames)
+
+            writer.writerow({"Initials": initials, "Score": score})
+
 
 if __name__ == "__main__":
     engine = TetrisEngine(debug=True)
+    engine._prepare_leaderboard_file()
